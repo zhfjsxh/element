28b261def6d97f63635bada8587eb8b68b46bdc2
ivanleo@qq.com <ivanleo@qq.com>
2021-03-23 14:07:57 +0800

    modified About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,15 +10,15 @@
 				<i class="el-icon-time"></i>
 				<span style="margin-left: 10px">{{scope.$index + 1}}</span>
 			</template>
-		</el-table-column>
-		<el-table-column label="日期" width="180">
-			<template slot-scope="scope">
+</el-table-column>
+<el-table-column label="日期" width="180">
+    <template slot-scope="scope">
 					<i class="el-icon-time"></i>
 					<span style="margin-left: 10px">{{ scope.row.date }}</span>
 				</template>
-		</el-table-column>
-		<el-table-column label="姓名" width="80">
-			<template slot-scope="scope">
+</el-table-column>
+<el-table-column label="姓名" width="80">
+    <template slot-scope="scope">
 				<el-popover  placement="top">
 					<!-- <p>姓名: {{ scope.row.name }}</p> -->
 					<!-- <p>住址: {{ scope.row.address }}</p> -->
@@ -27,9 +27,9 @@
 					</div>
 				</el-popover>
 				</template>
-		</el-table-column>
-		<el-table-column label="住址" width="250">
-			<template slot-scope="scope">
+</el-table-column>
+<el-table-column label="住址" width="250">
+    <template slot-scope="scope">
 				<el-popover  placement="top">
 					<!-- <p>姓名: {{ scope.row.name }}</p> -->
 					<!-- <p>住址: {{ scope.row.address }}</p> -->
@@ -38,20 +38,20 @@
 					</div>
 				</el-popover>
 				</template>
-		</el-table-column>
-		<el-table-column label="住址0002" width="250">
-			<template slot-scope="scope">
+</el-table-column>
+<el-table-column label="住址0002" width="250">
+    <template slot-scope="scope">
 				<el-popover  placement="top">
 					<!-- <p>姓名: {{ scope.row.name }}</p> -->
 					<!-- <p>住址: {{ scope.row.address }}</p> -->
 					<div slot="reference" class="name-wrapper">
-					<el-tag size="medium">{{ scope.row.address }}000000555</el-tag>
+					<el-tag size="medium">{{ scope.row.address }}000000</el-tag>
 					</div>
 				</el-popover>
 				</template>
-		</el-table-column>
-		<el-table-column label="操作">
-			<template slot-scope="scope">
+</el-table-column>
+<el-table-column label="操作">
+    <template slot-scope="scope">
 			<el-button
 				size="mini"
 				@click="handleEdit(scope.$index, scope.row)">编辑</el-button>
@@ -60,7 +60,7 @@
 				type="danger"
 				@click="handleDelete(scope.$index, scope.row)">删除</el-button>
 			</template>
-		</el-table-column>
+</el-table-column>
 </el-table>
 </div>
 </template>
