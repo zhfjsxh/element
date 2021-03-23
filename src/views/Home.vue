9e09b8ef6626a6f43822d0c1481fce43c70ca5ab
zhang <592207403@qq.com>
2021-03-23 19:31:12 +0800

    增加一个文章列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@
     <el-menu-item-group>
         <!-- <template slot="title">分组一</template> -->
         <el-menu-item index="/user">用户列表</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="/logbook">文章列表</el-menu-item>
     </el-menu-item-group>
     <el-menu-item-group title="">
         <el-menu-item index="2-3">选项3</el-menu-item>
---
+++ src/views/logbook.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="contianer">
+        <div class="banner">
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column prop="date" label="#" width="180" type="index"> </el-table-column>
+                <el-table-column prop="name" label="标题" width="180">
+                     <template slot-scope="scope">
+                         <span @click="getlist(scope.row.id)">{{scope.row.name || ''}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="copywriter" label="描述"> </el-table-column>
+                <el-table-column prop="copywriter" label="图片"> 
+                    <template slot-scope="scope">
+                        <el-image style="width: 50px; height: 50px" :src="scope.row.picUrl"></el-image>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>        
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name: 'logbook',
+    data(){
+        return {
+            tableData: [],                                              // 歌单标哥
+        }
+    },
+    created(){
+        this.init();
+    },
+    methods: {
+        init(){
+            axios.get('http://muisc-api.zxzx.shop/personalized?limit=10').then((res) => {
+                this.tableData = res.data.result || [];
+                console.log('页面首次加载数据res',res.data.result);
+            });
+        },
+        // 获取歌单详情
+        getlist(){
+            console.log('[获取歌单详情数据事件]');
+        },
+    }
+
+}
+</script>
