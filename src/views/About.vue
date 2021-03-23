616aab55658e67efbd9424509fd667a97200d049
ivanleo@qq.com <ivanleo@qq.com>
2021-03-23 19:20:36 +0800

    去掉button

--- src/views/About.vue
+++ src/views/About.vue
@@ -50,10 +50,6 @@
 				size="mini"
 				type="danger"
 				@click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                    <el-button
-                    size="mini"
-                    type="danger"
-                    @click="handleDelete(scope.$index, scope.row)">关闭呀</el-button>
 			</template>
 </el-table-column>
 </el-table>
