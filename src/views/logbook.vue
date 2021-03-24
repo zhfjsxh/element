5e39e51cf459e9a6614ecb4497b14996ae78230c
ivanleo@qq.com <ivanleo@qq.com>
2021-03-24 09:01:59 +0800

    0324第一次提交

--- src/views/logbook.vue
+++ src/views/logbook.vue
@@ -7,41 +7,41 @@
                      <template slot-scope="scope">
                          <span @click="getlist(scope.row.id)">{{scope.row.name || ''}}</span>
                     </template>
-                </el-table-column>
-                <el-table-column prop="copywriter" label="描述"> </el-table-column>
-                <el-table-column prop="copywriter" label="图片"> 
-                    <template slot-scope="scope">
+</el-table-column>
+<el-table-column prop="copywriter" label="描述"> </el-table-column>
+<el-table-column prop="copywriter" label="图片">
+    <template slot-scope="scope">
                         <el-image style="width: 50px; height: 50px" :src="scope.row.picUrl"></el-image>
                     </template>
-                </el-table-column>
-            </el-table>
-        </div>        
-    </div>
+</el-table-column>
+</el-table>
+</div>
+</div>
 </template>
 <script>
-import axios from 'axios'
-export default {
-    name: 'logbook',
-    data(){
-        return {
-            tableData: [],                                              // 歌单标哥
+    import axios from 'axios'
+    export default {
+        name: 'logbook',
+        data() {
+            return {
+                tableData: [], // 歌单标哥
+            }
+        },
+        created() {
+            this.init();
+        },
+        methods: {
+            init() {
+                axios.get('http://muisc-api.zxzx.shop/personalized?limit=10').then((res) => {
+                    this.tableData = res.data.result || [];
+                    console.log('页面首次加载数据res', res.data.result);
+                });
+            },
+            // 获取歌单详情
+            getlist() {
+                console.log('[获取歌单详情数据事件]');
+            },
         }
-    },
-    created(){
-        this.init();
-    },
-    methods: {
-        init(){
-            axios.get('http://muisc-api.zxzx.shop/personalized?limit=10').then((res) => {
-                this.tableData = res.data.result || [];
-                console.log('页面首次加载数据res',res.data.result);
-            });
-        },
-        // 获取歌单详情
-        getlist(){
-            console.log('[获取歌单详情数据事件]');
-        },
-    }
 
-}
+    }
 </script>
